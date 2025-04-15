276d394163017f4e01e58e761b9e5a247ca08b9b
Philipp Wissmann <philipp.wissmann@ethz.ch>
2025-04-15 08:43:36 +0200

    test(urlaction): add test for auth token

--- src/config/job-config/actions/urlaction/urlaction.interface.ts
+++ src/config/job-config/actions/urlaction/urlaction.interface.ts
@@ -5,7 +5,7 @@ export interface URLJobActionOptions {
   url: string;
   method?: "GET" | "POST" | "PUT" | "DELETE";
   headers?: Record<string, string>;
-  authTokenEnvVar?: "";
+  authTokenEnvVar?: string;
   body?: unknown;
 }
 
